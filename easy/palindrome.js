81ce33a0971afadde537b2693165f9bd5a9cccc9
ilyass <ilyass.oudli@gmail.com>
2025-08-22 23:54:34 -0400

    adding palindrome

--- easy/palindrome.js
+++ easy/palindrome.js
@@ -3,5 +3,9 @@
  * @return {boolean}
  */
 var isPalindrome = function (x) {
-    
+  //   const s = String(x);
+  //   return s === s.split().reverse().join("");
+
 };
+
+console.log(isPalindrome(113311));
