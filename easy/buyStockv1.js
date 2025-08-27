4b96c8cf08c08eabcd8c3a7847a572602c79fa4f
ilyass <ilyass.oudli@gmail.com>
2025-08-27 01:53:08 -0400

    more Practice for Math object with min and max methods

--- easy/buyStockv1.js
+++ easy/buyStockv1.js
@@ -16,3 +16,22 @@ var maxProfit = function (prices) {
 console.log(maxProfit([7, 1, 5, 3, 6, 4]));
 console.log(maxProfit([7, 6, 4, 3, 1]));
 console.log(maxProfit([2, 4, 1]));
+console.log(Math.min(34, 23));
+console.log(Math.max(34, 23));
+const arr1 = [7, 1, 5, 3, 6, 4];
+const arr2 = [23, 56, 3, 45, 7, 4];
+const maxPrice1 = Math.max(...arr1);
+const maxPrice2 = Math.max(...arr2);
+console.log(
+  "the max price on arr1 is " +
+    maxPrice1 +
+    " and is located in the index: " +
+    arr1.indexOf(maxPrice1)
+);
+console.log(
+    "the max price on arr2 is " +
+      maxPrice2 +
+      " and is located in the index: " +
+      arr2.indexOf(maxPrice2)
+  );
+console.log(Math.max(...arr1, ...arr2));
